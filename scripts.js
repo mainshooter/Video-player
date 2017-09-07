c9d3e61d8095bebdf0a9569af4e4a6b44061272d
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-07 13:48:50 +0200

    Added method to set the speed

--- scripts.js
+++ scripts.js
@@ -4,45 +4,69 @@ var Video;
 
 var videoElement = select('video');
 
-var videoStatus = false;
-// If the video is playing or not
 
 function select(element) {
   return(document.querySelector(element));
 }
-
+function selectAll(elements) {
+  return(document.querySelectorAll(elements));
+}
 (function() {
   Start = {
     placeListners: function() {
       select('.player__button').addEventListener('click', function(){ VideoController.playOrStop(); });
       select('.player__slider').addEventListener('mouseup', function() { VideoController.volume(); });
+      selectAll('.player__slider')[1].addEventListener('mouseup', function() { VideoController.speed(); });
     }
   }
 })();
 (function() {
   VideoController = {
+
+    /**
+     * Gets the status fo the video, is it playing or not
+     * @return {[boolean]} [If it is playing we return true if it isn't playing we return false]
+     */
+    status: function() {
+      var videoStatus = videoElement.paused;
+      if (videoStatus === true) {
+        // Video is paused we return false
+        return(false);
+      }
+
+      else {
+        // video is playing we return true
+        return(true);
+      }
+    },
     /**
      * Plays of pause the video
      */
     playOrStop: function() {
-      console.log("RUN");
-      if (videoStatus === true) {
+      if (VideoController.status() === true) {
         // Video is playing
         videoElement.pause();
         videoStatus = false;
       }
 
-      else if (videoStatus === false) {
+      else if (VideoController.status() === false) {
         // Video is not playing
         videoElement.play();
         videoStatus = true;
       }
     },
 
+    /**
+     * Controlls the volume of a video
+     */
     volume: function() {
       var volumeLvl = select('.player__slider').value;
-      console.log(volumeLvl);
       videoElement.volume = volumeLvl;
+    },
+
+    speed: function() {
+      var speed = selectAll('.player__slider')[1].value;
+      videoElement.playbackRate = speed;
     }
   }
 })();
